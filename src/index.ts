22bebaf8a6bb8c890b82898880884e750f71bc96
Raziel <3615018+rraziel@users.noreply.github.com>
2018-04-25 02:41:33 +0200

    Added missing AnnotationConfigApplicationContext export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export {ApplicationContext, ApplicationContextSettings} from './context';
+export {AnnotationConfigApplicationContext, ApplicationContext, ApplicationContextSettings} from './context';
 export {Component, ComponentScan, Configuration, Controller, Import, Inject, Named, PostConstruct, PreDestroy, Repository, Scope, Service, Value} from './decorators';
 export {ScopeType} from './metadata';
