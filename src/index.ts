e5b5be4607ec89dbca75a86d0fb60b044433d21b
Raziel <3615018+rraziel@users.noreply.github.com>
2018-05-06 18:51:16 +0200

    Added missing ElementClass decorator export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export {AnnotationConfigApplicationContext, ApplicationContext, ApplicationContextSettings} from './context';
-export {Component, ComponentScan, Conditional, Configuration, Controller, Import, Inject, Named, Order, PostConstruct, PreDestroy, Repository, Scope, Service, Value} from './decorators';
+export {Component, ComponentScan, Conditional, Configuration, Controller, ElementClass, Import, Inject, Named, Order, PostConstruct, PreDestroy, Repository, Scope, Service, Value} from './decorators';
 export {Condition, ConditionContext, ScopeType} from './metadata';
